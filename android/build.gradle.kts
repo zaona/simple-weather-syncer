56c61e6bdcec554a260b11678d13e27f970259c0
Zaona <3409473369@qq.com>
2025-11-02 13:51:57 +0800

    chore: 更新gradle构建配置

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,16 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
 subprojects {
     project.evaluationDependsOn(":app")
 }
